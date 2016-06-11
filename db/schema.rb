afcd22da88c0e2602cd6a36af3ce5fffced204b4
Zeina <zeemakky@gmail.com>
2016-06-11 12:06:05 -0500

    added breed model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611164135) do
+ActiveRecord::Schema.define(version: 20160611170538) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "age"
+    t.string   "breed"
   end
 
 end
